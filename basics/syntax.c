64e2bc1ac40ebcefb82a1a53f5b6886e82bb2fbb
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-06-02 20:14:24 +0530

    input/output

---
+++ basics/syntax.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Include standard input/output header
+
+// Main function: program execution starts here
+int main() {
+    // Print a message to the console
+    printf("Hello, World!\n");
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
