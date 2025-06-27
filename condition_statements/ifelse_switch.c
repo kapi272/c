535971026fe0780f36a99142f966873d8a5362fb
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-06-27 10:40:10 +0530

    Update

--- condition_statements/ifelse_switch.c
+++ condition_statements/ifelse_switch.c
@@ -1,15 +1,33 @@
-if-else(Syntax)
+/*                    if-else(Syntax)
 
-if(CONDITION){
-    // Code to execute if CONDITION is true
-} else {
-    // Code to execute if CONDITION is false
+                    if(CONDITION){
+                        // Code to execute if CONDITION is true
+                    } else {
+                        // Code to execute if CONDITION is false
+                    }
+
+
+                    else if (condition 1) {
+                        // Code to execute if CONDITION is true
+                    } else(condition 2){
+                        // Code to execute if all previous conditions are false
+                    }
+*/
+
+// program to find upper case or lower case
+
+#include<stdio.h>
+ 
+int main(){
+    char ch;
+    printf("Enter the Chacter\n");
+    scanf("%c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+        printf("Enter chacter is Upper case\n");
+    else if(ch >= 'a' && ch <= 'z')
+        printf("Enter chacter is Lower case\n");
+    else
+        printf("Not an english alphabet\n");
+    
 }
-
-
-else if (condition 1) {
-    // Code to execute if CONDITION is true
-} else(condition 2){
-    // Code to execute if all previous conditions are false
-}
-
--- condition_statements/switch.c
+++ condition_statements/switch.c
@@ -23,26 +23,67 @@
 
 // Marks calculater
 
+/*                                #include<stdio.h>
+                                int mark;
+                                int main(){
+                                    printf("Enter the marks\n");
+                                    scanf("%d", &mark);
+
+                                    
+                                    if(mark > 90 && mark < 100)
+                                        printf("a+\n");
+
+                                    if(mark < 90 && mark > 70)
+                                        printf("a\n");
+
+                                    if(mark < 70 && mark > 50)
+                                        printf("b\n");
+
+                                    if(mark < 40)
+                                        printf("Devare gathi odu appa hudugirana bittu\n");
+
+                                    else
+                                        printf("Enter valid marks\n");
+
+                                }
+*/
+
 #include<stdio.h>
-int mark;
+
 int main(){
-    printf("Enter the marks\n");
-    scanf("%d", &mark);
+    int marks;
+    printf("Enter the marks scored by std\n");
+    scanf("%d", &marks);
 
-    
-    if(mark > 90 && mark < 100)
-        printf("a+\n");
-
-    if(mark < 90 && mark > 70)
-        printf("a\n");
-
-    if(mark < 70 && mark > 50)
-        printf("b\n");
-
-    if(mark < 40)
-        printf("Devare gathi odu appa hudugirana bittu\n");
-
-    else
+    switch (marks/10)
+    {
+    case 10:
+    case 9:
+        printf("A+\n");
+        break;
+    case 8:
+    case 7:
+        printf("A\n");
+        break;
+    case 6:
+    case 5:
+        printf("B\n");
+        break;
+    case 4:
+        printf("c\n");
+        break;
+    case 3:
+    case 2:
+    case 1:
+        printf("Please read HARD, try next time\n");
+        break;
+    default:
         printf("Enter valid marks\n");
+    }
 
 }
+
+/*Limitation of this is if we enter the singal digit MARKS it will 
+show defaault statement says "Enter valid marks, its just a triyal i 
+did i can do it with switch rather than the "IF" op
+*/ 
