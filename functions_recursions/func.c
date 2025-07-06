b454eb0c074291d606bd9c45bb7bf45bb3b84115
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-07-06 23:32:26 +0530

    function call

---
+++ functions_recursions/func.c
@@ -0,0 +1,18 @@
+// function call for the hello word
+
+
+void printfunc(); //function declaration
+
+
+#include<stdio.h>
+int main(){
+
+    printfunc(); //function decleration
+    return 0;
+}
+
+
+//function definition
+void printfunc(){
+    printf("hello world !!!\n");
+}
