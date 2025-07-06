55610db88ab2e552e8fcf41fb2538a9687b37f25
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-07-06 23:26:03 +0530

    Add functions and recursion examples

---
+++ functions_recursions/func_recursion.c
@@ -0,0 +1,15 @@
+// functions definition and recursion definition
+
+// Function: A function is a block of code that performs a specific task, can take input, and can return a value.
+// Syntax example:
+// return_type function_name(parameter_list) {
+//     // function body
+// }
+
+// Recursion: Recursion is a programming technique where a function calls itself directly or indirectly to solve a problem.
+// Example:
+// int factorial(int n) {
+//     if (n == 0) return 1;
+//     else return n * factorial(n - 1);
+//  }
+
