ec6bc37986c1c490006e45b420816e25e03f2757
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-07-11 11:51:28 +0530

    resursion

---
+++ functions_recursions/recursions/rec.c
@@ -0,0 +1,11 @@
+ /*
+ Recursion: Recursion is a programming technique where a 
+ function calls itself directly or indirectly to solve a problem.
+ Example:
+ int factorial(int n) {
+     if (n == 0) return 1;
+     else return n * factorial(n - 1);
+    }
+*/
+//=============== IGNORE ================//
+
