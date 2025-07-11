8a40dda851a96fc4786512309a572fdae81c9712
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-07-11 12:01:49 +0530

    recursion

---
+++ functions_recursions/recursions/recsersive.c
@@ -0,0 +1,19 @@
+// print 5 times hello by recursive function
+
+#include<stdio.h>
+void printHW(int count);
+
+int main(){
+    printHW(5);
+    return 0;
+}
+
+//recursive function
+
+void printHW(int count){
+    if(count == 0){
+        return ;
+    }
+    printf("Hello world\n");
+    printHW(count - 1);
+}
