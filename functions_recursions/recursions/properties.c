da02d317f5c548a63a4272a6ee8264756398a666
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-07-17 16:35:34 +0530

    recrsion_properties

---
+++ functions_recursions/recursions/properties.c
@@ -0,0 +1,23 @@
+// properties of recursive functions
+// 1. A recursive function must have a base case.
+// 2. Recursive functions can be less efficient than iterative functions.
+// 3. Each recursive call adds a new layer to the call stack.
+// 4. Recursive functions can be elegant and easier to read.
+
+/*
+
+i) anything that can be done iteratively can also be done recursively
+
+ii) recursive functions can sometimes be more elegant and easier to read than their
+    iterative counterparts and can " lead to simpler code "
+
+iii) BASE CASES are essential in recursive functions to prevent infinite recursion
+    and stack overflow errors (base function stops recursion)
+
+
+iv) recursive functions can be less efficient than iterative functions due to the overhead
+    of multiple function calls and maintaining the call stack
+
+v) iteration have infinite loops & recursive has stack overflow errors
+
+*/
