e381e351b513184a8f33be4e7ec493bb12682609
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-10 14:53:24 +0530

    pointers

---
+++ pointers/poi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int age;
+    int *pointer;
+
+    printf("Enter the age of person\n");
+    scanf("%d", &age);
+    
+    pointer = &age;  // Assign address of age to pointer
+    
+    printf("Value of age: %d\n", age);
+    printf("Address of age: %p\n", &age);
+    printf("Value pointed by pointer: %d\n", *pointer);
+    printf("Address stored in pointer: %p\n", pointer);
+    
+    return 0;
+}
