a999fcb8ca81357890b1663c4d93afe875f57885
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-11 18:55:15 +0530

    Call by value vs refrence

---
+++ pointers/call_by_value_and_reference.c
@@ -0,0 +1,6 @@
+// call by value vs call by reference
+// call by value: a copy of the actual parameter is passed
+// call by reference: address of the actual parameter is passed
+//======================================================================================================//
+// In call by value, changes made to the parameter inside the function do not affect the actual argument.
+// In call by reference, changes made to the parameter inside the function do affect the actual argument.   
