dafa94fb60676677ed4cd09a61ecad5d338aba5a
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-11 19:04:51 +0530

    Call by value

---
+++ pointers/call_by_value.c
@@ -0,0 +1,22 @@
+//a copy of variables are made and passed to the function
+
+#include<stdio.h>
+
+void sqer(int x); // USER DEFINED FUNCTION
+
+int number; // golbel decleration of varibels so that it can acessed in user defined functions
+
+int main(){
+
+    printf("Enter the number :");
+    scanf("%d", &number);
+
+    sqer(number);
+    return 0;
+
+}
+
+void sqer(int x){
+    x = x * x;
+    printf("Squear of %d : %d\n", number, x);
+}
