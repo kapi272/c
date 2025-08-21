34993850d11b492d6b8a48d0147a1b6120458b3a
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-21 18:07:05 +0530

    Arrays

---
+++ arrays/array.c
@@ -0,0 +1,18 @@
+//==================================================================================================================//
+//               Collection of similar data types stored at a "contiguous memory" location                          //
+//==================================================================================================================//
+/*
+    -> Should always use the elements within the INDEX VALUE other wise will give " run time error "
+
+    -> c is zero based indexing language
+
+    -> if array length is ' n ' then valid index is ' 0 ' to ' n-1 '
+
+    -> DataType array[Size of array];
+       ex : int mark[10];
+
+    -> input    scanf("%DataType", &array[index]);
+
+    -> output   printf("%DataType", array[index]);
+
+*/
