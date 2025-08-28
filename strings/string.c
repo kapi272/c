9ce130f08df117998b76b48edf025891fdc3c368
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-28 15:26:00 +0530

    strings

---
+++ strings/string.c
@@ -0,0 +1,7 @@
+// a character array terminated by a null character "\0"
+// null character denotes the end of the string
+//
+//ex
+//char name[] = {'s', 'h', 'y', 'a', 'm', '\0'};
+//char name1 = "saraswathi";
+//char collagname[] = "the beast";
