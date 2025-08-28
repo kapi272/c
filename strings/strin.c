1d189993510998433cb82cb9875155e6e53c0a3d
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-28 16:31:05 +0530

    update

---
+++ strings/strin.c
@@ -0,0 +1,11 @@
+//dimpel string to tack input as name
+
+#include<stdio.h>
+
+int main(){
+    char name[50];
+    printf("Enter the name \n");
+    scanf("%s", name);
+    printf("Name is : %s\n", name);
+    return 0;
+}
