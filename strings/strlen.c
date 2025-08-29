e2b48f43582c2366f0675e87b23d941b24308b3a
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-08-29 16:36:47 +0530

    update

---
+++ strings/strlen.c
@@ -0,0 +1,10 @@
+// using the strlen function
+// used to find length of string
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char family[] = "Dechamma";
+    printf("Length of string : %lu\n", strlen(family));
+    return 0;
+}
