ccc95ffeca45bd1ae92b7b4412cc8890922d8392
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-09-04 15:30:50 +0530

    Structures

---
+++ structures/first.c
@@ -0,0 +1,28 @@
+//a collection of values of diffrent data types
+
+//Syntax
+
+/*
+struct structuer_name{
+
+    data_type var_name[size];
+
+    data_type var_name;
+
+    data_type var_name;
+
+};
+*/
+
+/*
+
+EX,
+
+struct student{
+    char name[50];
+
+    int roll_no;
+
+    float CGPA;
+}
+*/
