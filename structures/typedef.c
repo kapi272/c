7503eab976bba7ffeeb75240e157ca790728899e
M shyam preasad <106077425+kapi272@users.noreply.github.com>
2025-09-05 13:18:21 +0530

    Typedef Keyword

---
+++ structures/typedef.c
@@ -0,0 +1,21 @@
+// Typedef KeyWord
+
+// Used to creat 'ALIAS' for data types
+
+// EX
+
+/*
+
+
+        typedef struct student{
+        
+        int roll;
+        char name[];
+        float Cgpa;
+        
+    } std;
+
+
+*/
+
+// instade of writing struct student s1 ______    we can write std s1
